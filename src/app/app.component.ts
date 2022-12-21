fc69d7404de408e4774409a72e441fbc54cb1524
med4u <med4u@users.noreply.github.com>
2022-12-21 20:05:00 +0100

    5 files modified, 1 added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,46 @@
 import { Component, VERSION } from '@angular/core';
+import { NgxChartsModule } from '@swimlane/ngx-charts';
+import { multi,data } from './data';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+export class AppComponent {
+  name = 'HEM_Charts ' ;
+  multi: any[];
+  view: any[] = [700, 300];
+
+  // options
+  legend: boolean = true;
+  showLabels: boolean = true;
+  animations: boolean = true;
+  xAxis: boolean = true;
+  yAxis: boolean = true;
+  showYAxisLabel: boolean = true;
+  showXAxisLabel: boolean = true;
+  xAxisLabel: string = 'Year';
+  yAxisLabel: string = 'Population';
+  timeline: boolean = true;
+
+  colorScheme = {
+    domain: ['#5AA454', '#E44D25', '#CFC0BB', '#7aa3e5', '#a8385d', '#aae3f5'],
+  };
+
+  constructor() {
+    Object.assign(this, { multi ,data});
+  }
+
+  onSelect(data): void {
+    console.log('Item clicked', JSON.parse(JSON.stringify(data)));
+  }
+
+  onActivate(data): void {
+    console.log('Activate', JSON.parse(JSON.stringify(data)));
+  }
+
+  onDeactivate(data): void {
+    console.log('Deactivate', JSON.parse(JSON.stringify(data)));
+  }
 }
---
+++ src/app/data.ts
@@ -0,0 +1,300 @@
+export const data = [
+  {
+    idScod: 1144630,
+    libelle: 'Poids',
+    libelleComplementaire: '',
+    code: 'POIDS',
+    couleur: '000000',
+    unite: 'KG',
+    points: [
+      {
+        value: 5.5,
+        date: 1537368328000,
+        dateSaisie: 1537368328000,
+        utilisateur: 'X',
+        commentaire: null,
+        numOccurrence: null,
+        idScod: 1144630,
+        courbesConstanteEtabKey: {
+          idScod: 1144630,
+        },
+      },
+      {
+        value: 6.45,
+        date: 1537941600000,
+        dateSaisie: 1537950309000,
+        utilisateur: 'Y',
+        commentaire: null,
+        numOccurrence: 1,
+        idScod: 1144630,
+        courbesConstanteEtabKey: {
+          idScod: 1144630,
+        },
+      },
+      {
+        value: 7.05,
+        date: 1548090000000,
+        dateSaisie: 1548094856000,
+        utilisateur: 'P',
+        commentaire: null,
+        numOccurrence: 1,
+        idScod: 1144630,
+        courbesConstanteEtabKey: {
+          idScod: 1144630,
+        },
+      },
+      {
+        value: 12.9,
+        date: 1605632400000,
+        dateSaisie: 1605635181000,
+        utilisateur: 'G',
+        commentaire: null,
+        numOccurrence: 1,
+        idScod: 1144630,
+        courbesConstanteEtabKey: {
+          idScod: 1144630,
+        },
+      },
+      {
+        value: 17.2,
+        date: 1665381600000,
+        dateSaisie: 1665381477000,
+        utilisateur: 'A',
+        commentaire: null,
+        numOccurrence: null,
+        idScod: 1144630,
+        courbesConstanteEtabKey: {
+          idScod: 1144630,
+        },
+      },
+      {
+        value: 17.6,
+        date: 1670259600000,
+        dateSaisie: 1670258088000,
+        utilisateur: 'L',
+        commentaire: null,
+        numOccurrence: null,
+        idScod: 1144630,
+        courbesConstanteEtabKey: {
+          idScod: 1144630,
+        },
+      },
+    ],
+    norme: null,
+    numOrdreLiees: null,
+    libelleLiees: null,
+    constanteCumulee: false,
+  },
+  {
+    idScod: 1144632,
+    libelle: 'Taille',
+    libelleComplementaire: '',
+    code: 'TAILLE',
+    couleur: '00CC00',
+    unite: 'CM',
+    points: [
+      {
+        value: 61,
+        date: 1537368328000,
+        dateSaisie: 1537368328000,
+        utilisateur: 'N',
+        commentaire: null,
+        numOccurrence: null,
+        idScod: 1144632,
+        courbesConstanteEtabKey: {
+          idScod: 1144632,
+        },
+      },
+      {
+        value: 61,
+        date: 1538378880000,
+        dateSaisie: 1538378888000,
+        utilisateur: 'D',
+        commentaire: null,
+        numOccurrence: 1,
+        idScod: 1144632,
+        courbesConstanteEtabKey: {
+          idScod: 1144632,
+        },
+      },
+      {
+        value: 73,
+        date: 1564740540000,
+        dateSaisie: 1564740585000,
+        utilisateur: 'R',
+        commentaire: null,
+        numOccurrence: 1,
+        idScod: 1144632,
+        courbesConstanteEtabKey: {
+          idScod: 1144632,
+        },
+      },
+      {
+        value: 77.5,
+        date: 1589263200000,
+        dateSaisie: 1589283332000,
+        utilisateur: 'C',
+        commentaire: null,
+        numOccurrence: 1,
+        idScod: 1144632,
+        courbesConstanteEtabKey: {
+          idScod: 1144632,
+        },
+      },
+      {
+        value: 80.7,
+        date: 1591005600000,
+        dateSaisie: 1591009763000,
+        utilisateur: 'R',
+        commentaire: null,
+        numOccurrence: 1,
+        idScod: 1144632,
+        courbesConstanteEtabKey: {
+          idScod: 1144632,
+        },
+      },
+      {
+        value: 81,
+        date: 1596175200000,
+        dateSaisie: 1596181396000,
+        utilisateur: 'F',
+        commentaire: null,
+        numOccurrence: 1,
+        idScod: 1144632,
+        courbesConstanteEtabKey: {
+          idScod: 1144632,
+        },
+      },
+      {
+        value: 82,
+        date: 1602741600000,
+        dateSaisie: 1602758999000,
+        utilisateur: 'K',
+        commentaire: null,
+        numOccurrence: 1,
+        idScod: 1144632,
+        courbesConstanteEtabKey: {
+          idScod: 1144632,
+        },
+      },
+      {
+        value: 84,
+        date: 1604386800000,
+        dateSaisie: 1604396898000,
+        utilisateur: 'F',
+        commentaire: null,
+        numOccurrence: 1,
+        idScod: 1144632,
+        courbesConstanteEtabKey: {
+          idScod: 1144632,
+        },
+      },
+      {
+        value: 85,
+        date: 1606892400000,
+        dateSaisie: 1606903586000,
+        utilisateur: 'C',
+        commentaire: null,
+        numOccurrence: 1,
+        idScod: 1144632,
+        courbesConstanteEtabKey: {
+          idScod: 1144632,
+        },
+      },
+      {
+        value: 96,
+        date: 1661702400000,
+        dateSaisie: 1661723657000,
+        utilisateur: 'B',
+        commentaire: 'taille début Août 2022',
+        numOccurrence: null,
+        idScod: 1144632,
+        courbesConstanteEtabKey: {
+          idScod: 1144632,
+        },
+      },
+      {
+        value: 96,
+        date: 1662012000000,
+        dateSaisie: 1662020279000,
+        utilisateur: 'L',
+        commentaire: null,
+        numOccurrence: null,
+        idScod: 1144632,
+        courbesConstanteEtabKey: {
+          idScod: 1144632,
+        },
+      },
+      {
+        value: 97,
+        date: 1664604000000,
+        dateSaisie: 1664619700000,
+        utilisateur: 'P',
+        commentaire: null,
+        numOccurrence: null,
+        idScod: 1144632,
+        courbesConstanteEtabKey: {
+          idScod: 1144632,
+        },
+      },
+      {
+        value: 98,
+        date: 1667372400000,
+        dateSaisie: 1667376701000,
+        utilisateur: 'E',
+        commentaire: null,
+        numOccurrence: null,
+        idScod: 1144632,
+        courbesConstanteEtabKey: {
+          idScod: 1144632,
+        },
+      },
+      {
+        value: 99,
+        date: 1669879200000,
+        dateSaisie: 1669879223000,
+        utilisateur: 'A',
+        commentaire: null,
+        numOccurrence: null,
+        idScod: 1144632,
+        courbesConstanteEtabKey: {
+          idScod: 1144632,
+        },
+      },
+    ],
+    norme: null,
+    numOrdreLiees: null,
+    libelleLiees: null,
+    constanteCumulee: false,
+  },
+];
+
+function extract_for_chart(...data) {
+  let data_for_chart;
+  for (let x of data) {
+    for (let y of x) {
+      if (y.code == 'POIDS') {
+        console.log(y);
+        for (let poids of y.points) {
+          console.log(
+            poids.value +
+              ' kg le ' +
+              new Date(poids.date).toLocaleDateString('fr')
+          );
+        }
+      }
+      if (y.code == 'TAILLE') {
+        console.log(y);
+        for (let taille of y.points) {
+          console.log(
+            taille.value +
+              ' cm le ' +
+              new Date(taille.date).toLocaleDateString('fr')
+          );
+        }
+      }
+    }
+  }
+}
+
+export var multi = extract_for_chart(data);
